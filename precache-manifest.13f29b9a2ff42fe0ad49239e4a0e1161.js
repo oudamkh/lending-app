7d80bdcfbe2dd4613c146b0575baf93a3913367f
oudam.seng <oudam.seng@wingmoney.com>
2020-01-21 21:36:03 +0700

    Updates

---
+++ precache-manifest.13f29b9a2ff42fe0ad49239e4a0e1161.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "509df359c4c0dff4425e",
+    "url": "/lending-app/WebLending/static/css/main.e11e8af0.chunk.css"
+  },
+  {
+    "revision": "7775acb5904a94838e87",
+    "url": "/lending-app/WebLending/static/js/2.065cbc90.chunk.js"
+  },
+  {
+    "revision": "d77f52e74caeeba764e0bdb673068340",
+    "url": "/lending-app/WebLending/static/js/2.065cbc90.chunk.js.LICENSE"
+  },
+  {
+    "revision": "509df359c4c0dff4425e",
+    "url": "/lending-app/WebLending/static/js/main.e4a2908c.chunk.js"
+  },
+  {
+    "revision": "56e62167dcc8abd0fbec",
+    "url": "/lending-app/WebLending/static/js/runtime-main.b198bd84.js"
+  },
+  {
+    "revision": "90556ad4c6e2f730e35220fc45b676ea",
+    "url": "/lending-app/WebLending/static/media/Battambang-Bold.90556ad4.woff2"
+  },
+  {
+    "revision": "ac08013a42ac205715ed38fd986e5da6",
+    "url": "/lending-app/WebLending/static/media/Battambang-Bold.ac08013a.woff"
+  },
+  {
+    "revision": "28b0b1021f9903941d450914f39bae21",
+    "url": "/lending-app/WebLending/static/media/Battambang-Regular.28b0b102.woff2"
+  },
+  {
+    "revision": "876479d2a436c67e25109caaed845b54",
+    "url": "/lending-app/WebLending/static/media/Battambang-Regular.876479d2.woff"
+  },
+  {
+    "revision": "4437686243eaa4ad20e9ecdec3f038cc",
+    "url": "/lending-app/WebLending/static/media/Bayon-Regular.44376862.woff2"
+  },
+  {
+    "revision": "e305d4ac6858b37a4d90621dd5c6d694",
+    "url": "/lending-app/WebLending/static/media/Bayon-Regular.e305d4ac.woff"
+  },
+  {
+    "revision": "63697d90642a27c25c1487a45190878c",
+    "url": "/lending-app/WebLending/static/media/Khmer.63697d90.woff"
+  },
+  {
+    "revision": "8e50dda0720354deb763ed2c8a373599",
+    "url": "/lending-app/WebLending/static/media/Khmer.8e50dda0.woff2"
+  },
+  {
+    "revision": "22f26af0c7f9e3892c3a81e20ab45c3f",
+    "url": "/lending-app/WebLending/static/media/Roboto-Black.22f26af0.woff2"
+  },
+  {
+    "revision": "595194dafb44cca9e48962e28ca7f1bd",
+    "url": "/lending-app/WebLending/static/media/Roboto-Black.595194da.woff"
+  },
+  {
+    "revision": "c8f2ac17e5fd216bc0f93f8019e5ceda",
+    "url": "/lending-app/WebLending/static/media/Roboto-BlackItalic.c8f2ac17.woff2"
+  },
+  {
+    "revision": "fca0061bf317ad26790f20e687ed3220",
+    "url": "/lending-app/WebLending/static/media/Roboto-BlackItalic.fca0061b.woff"
+  },
+  {
+    "revision": "3b20863e11a00f4dc5b3647ee31b6b3b",
+    "url": "/lending-app/WebLending/static/media/Roboto-Bold.3b20863e.woff2"
+  },
+  {
+    "revision": "5d67c8dbf3dd425cf90398c042bcbe4c",
+    "url": "/lending-app/WebLending/static/media/Roboto-Bold.5d67c8db.woff"
+  },
+  {
+    "revision": "6f2cf24f80f5f6c6360c4f137038cc9a",
+    "url": "/lending-app/WebLending/static/media/Roboto-BoldItalic.6f2cf24f.woff"
+  },
+  {
+    "revision": "f3a2677418b69847191892d4de222312",
+    "url": "/lending-app/WebLending/static/media/Roboto-BoldItalic.f3a26774.woff2"
+  },
+  {
+    "revision": "de9d3b6ae9ad6769bb1b8b19fd1a1026",
+    "url": "/lending-app/WebLending/static/media/Roboto-Italic.de9d3b6a.woff"
+  },
+  {
+    "revision": "f9ee50bef89784c206fa324802819da6",
+    "url": "/lending-app/WebLending/static/media/Roboto-Italic.f9ee50be.woff2"
+  },
+  {
+    "revision": "2d87e7f19ca7d0a1f0b411d3ccc30c19",
+    "url": "/lending-app/WebLending/static/media/Roboto-Light.2d87e7f1.woff"
+  },
+  {
+    "revision": "8d7611b03a7e90a08d38df37fa1be782",
+    "url": "/lending-app/WebLending/static/media/Roboto-Light.8d7611b0.woff2"
+  },
+  {
+    "revision": "dd6c9f6b29b1b32c2eff790903501a38",
+    "url": "/lending-app/WebLending/static/media/Roboto-LightItalic.dd6c9f6b.woff"
+  },
+  {
+    "revision": "ec8d9dab607aaa49b2c86a8184497d6b",
+    "url": "/lending-app/WebLending/static/media/Roboto-LightItalic.ec8d9dab.woff2"
+  },
+  {
+    "revision": "40c802e3a51ddde03eb4c36dbedb8c77",
+    "url": "/lending-app/WebLending/static/media/Roboto-Medium.40c802e3.woff"
+  },
+  {
+    "revision": "8fa6797ead4cf98699440338bbc12449",
+    "url": "/lending-app/WebLending/static/media/Roboto-Medium.8fa6797e.woff2"
+  },
+  {
+    "revision": "ce1baefaaf70e577ece34a760b8bffab",
+    "url": "/lending-app/WebLending/static/media/Roboto-MediumItalic.ce1baefa.woff2"
+  },
+  {
+    "revision": "fb3c59d8f524e5b52f8b32cce3defa58",
+    "url": "/lending-app/WebLending/static/media/Roboto-MediumItalic.fb3c59d8.woff"
+  },
+  {
+    "revision": "26a727d8c91494ad7c189a31fac4a91e",
+    "url": "/lending-app/WebLending/static/media/Roboto-Regular.26a727d8.woff"
+  },
+  {
+    "revision": "fd317bd152694dfa37a03aef78b76ec4",
+    "url": "/lending-app/WebLending/static/media/Roboto-Regular.fd317bd1.woff2"
+  },
+  {
+    "revision": "7495e6afb769fba56853cce679bf6b22",
+    "url": "/lending-app/WebLending/static/media/Roboto-Thin.7495e6af.woff"
+  },
+  {
+    "revision": "f3d101973b3a7ebf1746975a5e2e8393",
+    "url": "/lending-app/WebLending/static/media/Roboto-Thin.f3d10197.woff2"
+  },
+  {
+    "revision": "521afd33ccc31ec90ff20695d3813da4",
+    "url": "/lending-app/WebLending/static/media/Roboto-ThinItalic.521afd33.woff"
+  },
+  {
+    "revision": "feffec1c7d4fbbd15c36b64ce31b21d2",
+    "url": "/lending-app/WebLending/static/media/Roboto-ThinItalic.feffec1c.woff2"
+  },
+  {
+    "revision": "f8a8ed00367b9f13aec807ca23829b9f",
+    "url": "/lending-app/index.html"
+  }
+]);
